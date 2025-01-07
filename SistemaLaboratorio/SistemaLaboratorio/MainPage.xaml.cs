975b1ff8644b6aee24b530ecf81240289c36b40a
zurielmontanosanchez@outlook.com <zurielmontanosanchez@outlook.com>
2025-01-07 09:57:22 -0600

    Creacion de menú

--- SistemaLaboratorio/SistemaLaboratorio/MainPage.xaml.cs
+++ SistemaLaboratorio/SistemaLaboratorio/MainPage.xaml.cs
@@ -1,30 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+﻿using Windows.UI.Xaml.Controls;
 
 namespace SistemaLaboratorio
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            NavView.ItemInvoked += NavView_ItemInvoked;
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+                return;
+
+            var tag = args.InvokedItemContainer.Tag.ToString();
+
+            switch (tag)
+            {
+                case "home":
+                    // Navegar a la página de inicio
+                    // ContentFrame.Navigate(typeof(HomePage));
+                    break;
+                case "results":
+                    // Navegar a la página de resultados
+                    // ContentFrame.Navigate(typeof(ResultsPage));
+                    break;
+            }
         }
     }
 }
